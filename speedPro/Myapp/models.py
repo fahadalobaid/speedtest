f40146d74dd4f506f4061eb9aa87ac89bb4cd116
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-07-24 16:42:39 +0300

    end

--- speedPro/Myapp/models.py
+++ speedPro/Myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Speedtest(models.Model):
+    destination = models.CharField(max_length=100)
+    time = models.TimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+   
+
